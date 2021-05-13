e374ebd84bad72f4cc45d4179428386a0ad27372
Lars van der Niet <larsvanderniet@gmail.com>
2021-05-13 11:44:33 +0200

    Another test Vercel

--- public/nuxt/7632c16.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{327:function(e,n,r){"use strict";r.r(n);var o=r(235);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","8.3.1","app")}}]);
